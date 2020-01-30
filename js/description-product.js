0113a5e23077121d4f91242f2d3c850a641d1876
RafaelPiedade <rafaeldade@hotmail.com>
2020-01-30 10:02:29 -0200

    :construction: 014 Desafio fácil, intermediário e difícil

--- js/description-product.js
+++ js/description-product.js
@@ -9,5 +9,29 @@ function handleClick() {
     $heart.classList.add("-active");
   } */
 
-  $heart.classList.toggle('-active')
+  $heart.classList.toggle("-active");
 }
+
+
+/* document.querySelector(".-star").addEventListener("moeover", () => {
+  console.log($stars);
+}); */
+
+// pegar estrelas
+// mudar cor ao click na estrela
+
+const $stars = window.document.querySelectorAll(".-star .icon");
+for (let i = 0; i < $stars.length; i++) {
+  $stars[i].addEventListener("click", () => changeStars(i) );
+}
+
+function changeStars(number) {
+  for (let i = 0; i < $stars.length; i++) {
+    const element = $stars[i];
+    if(i <= number){
+      element.classList.add('-active')
+    }else{
+      element.classList.remove('-active')
+    }
+  }
+} 
