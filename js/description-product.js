301f4d23898ea69ff85f20bd42a87adcf9b92098
RafaelPiedade <rafaeldade@hotmail.com>
2020-01-31 15:54:39 -0200

    :checkered_flag: 016 Finished

--- js/description-product.js
+++ js/description-product.js
@@ -2,44 +2,22 @@ const $heart = window.document.querySelector(".-heart");
 const $stars = document.querySelectorAll(".star");
 
 $heart.addEventListener("click", handleClick);
-
-$stars.forEach($star => {
-  $star.addEventListener("click", handleClick);
+$stars.forEach(($star, index) => {
+  $star.addEventListener("click", () => {
+    handleClickStar(index);
+  });
 });
 
 function handleClick() {
   this.classList.toggle("-active");
 }
 
-/* document.querySelector(".-star").addEventListener("moeover", () => {
-  console.log($stars);
-}); */
-
-// pegar estrelas
-// mudar cor ao click na estrela
-
-/* const $stars = document.querySelectorAll(".star");
-for (let i = 0; i < $stars.length; i++) {
-  $stars[i].addEventListener("click", () => {
-    changeStars(i)
-  } );
-}
-function changeStars(number) {
-  for (let i = 0; i < $stars.length; i++) {
-    const element = $stars[i];
-    if(i <= number){
-      element.classList.add('-active')
-    }else{
-      element.classList.remove('-active')
+function handleClickStar(key) {
+  $stars.forEach(($star, index) => {
+    if (index <= key) {
+      $star.classList.add("-active");
+    } else {
+      $star.classList.remove("-active");
     }
-  }
-}  */
-
-/* 
-$stars[0].addEventListener("click", handleClickStar);
-
-function handleClickStar() {
-  this.classList.toggle("-active");
-  console.log(this)
+  });
 }
- */
