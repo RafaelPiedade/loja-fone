5260475d2e948f0638290cd56fd5257ad697c085
RafaelPiedade <rafaeldade@hotmail.com>
2020-02-02 12:27:30 -0200

    :checkered_flag: 018 Finished

--- js/header-store.js
+++
@@ -1,8 +0,0 @@
-const $search = document.querySelector(".search");
-const $icons = document.querySelectorAll(".header-store .icon");
-
-$icons.forEach($icon => {
-  $icon.addEventListener("click", () => {
-    $search.classList.toggle("-active");
-  });
-});
